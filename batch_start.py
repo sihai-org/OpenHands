df1eee6bb4f29f8c56c6ffa31c6ef5705f3ac182
CoreJa <i@coreja.com>
2025-05-11 05:26:05 +0800

    batch start containers

---
+++ batch_start.py
@@ -0,0 +1,95 @@
+import json
+import socket
+from pathlib import Path
+
+import docker
+import docker.errors
+import pandas as pd
+
+BASE_DIR = Path("ruic_32test")
+WORKSPACE_BASE = BASE_DIR / "workspace"
+ARTICLE_BASE = BASE_DIR / "articles"
+BASE_URL = "http://articles.datou.me"
+PORT_START = 9001
+
+docker_cli = docker.from_env()
+
+
+def docker_build(work_dir: Path, uuid_str: str):
+    return docker_cli.images.build(path=str(work_dir), tag=f"{uuid_str}:latest", rm=True)
+
+
+def docker_run(image: str, uuid_str: str, host_port: int, container_port: int):
+    container = docker_cli.containers.run(
+        image,
+        detach=True,
+        ports={f"{container_port}/tcp": host_port},
+        stdin_open=True,
+        tty=True,
+        auto_remove=True,
+        name=uuid_str,
+    )
+    return container
+
+
+def check_port_in_use(port: int):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind(("0.0.0.0", port))
+            return False  # 端口未被占用
+        except socket.error:
+            return True  # 端口被占用
+
+
+def get_expose_port(dockerfile_path: Path):
+    with open(dockerfile_path, "r") as f:
+        for line in f:
+            if line.startswith("EXPOSE"):
+                return int(line.split()[1])
+    raise ValueError(f"No exposed port for {dockerfile_path}")
+
+
+def main():
+    c2a = []
+    for dockerflie_path in WORKSPACE_BASE.glob("**/Dockerfile"):
+        uuid_str = dockerflie_path.parent.parent.name
+        print(f"Building docker image for {uuid_str}")
+        image_name = f"{uuid_str}:latest"
+        try:
+            image = docker_cli.images.get(name=image_name)
+        except docker.errors.ImageNotFound:
+            print(f"Image {image_name} not found, building...")
+            image = docker_build(dockerflie_path.parent, uuid_str)
+
+        print(f"got image {image}, trying to run...")
+
+        host_port = PORT_START
+        while check_port_in_use(host_port):
+            host_port += 1
+
+        print(f"Running docker container for {uuid_str} on port {host_port}")
+        container_port = get_expose_port(dockerflie_path)
+        container = docker_run(f"{uuid_str}:latest", uuid_str, host_port, container_port)
+        print(f"Container {container.id} is running...")
+
+        article_path = ARTICLE_BASE / f"{uuid_str}.json"
+        article = json.load(article_path.open())
+
+        c2a.append({
+            "uuid": article["uuid"],
+            "url": article["url"],
+            "title": article["title"],
+            "executable": article["executable"],
+            "complexity": article["complexity"],
+            "confidence": article["confidence"],
+            "cost": article["cost"]["cost"],
+            "input_tokens": article["cost"]["input_tokens"],
+            "output_tokens": article["cost"]["output_tokens"],
+            "host_url": f"{BASE_URL}:{host_port}",
+        })
+
+    pd.DataFrame(c2a).to_csv(BASE_DIR / "c2a.csv", index=False)
+
+
+if __name__ == '__main__':
+    main()
